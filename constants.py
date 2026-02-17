420ff856656d6c9c0536d611aa22b49092835a4b
Shiny S U <shiny.su@freshworks.com>
2026-02-17 09:54:34 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 SNYK_API_BASE = "https://api.snyk.io"
 API_VERSION = "2023-11-20"
-SNYK_TOKEN = "5423-77722-3829"
-ORG_ID = "ssssss"
+SNYK_TOKEN = "4ca4beee-baa0-436c-8ebb-2cdda994fd1b"
+ORG_ID = "839ad1c5-fe58-4bad-8323-df97f372c610"
